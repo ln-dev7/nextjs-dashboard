35e04014b331c55821e721659aaa118bd5bdec6c
ln-dev7 <leonelngoya@gmail.com>
2023-10-27 11:00:07 +0100

    chap 11

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,13 +1,11 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Image from "next/image";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchLatestInvoices } from "@/app/lib/data";
+import { LatestInvoice } from "@/app/lib/definitions";
+export default async function LatestInvoices() {
+  const latestInvoices: LatestInvoice[] = await fetchLatestInvoices();
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -22,10 +20,10 @@ export default async function LatestInvoices({
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
-                  },
+                    "border-t": i !== 0,
+                  }
                 )}
               >
                 <div className="flex items-center">
@@ -34,7 +32,7 @@ export default async function LatestInvoices({
                     className="mr-4 rounded-full"
                     width={32}
                     height={32}
-                    alt='image'
+                    alt="image"
                   />
                   <div className="min-w-0">
                     <p className="truncate text-sm font-semibold md:text-base">
